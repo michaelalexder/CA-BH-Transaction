0bbc97190cfeddf9cb7fdd26a29ca93771ae43b1
Mikhail Dergalev <mikhail.dergalev@crypterium.com>
2021-04-07 19:44:59 +0300

    Transaction service exception handler

--- src/main/java/com/bhca/transaction/exception/ControllerExceptionHandler.java
+++ src/main/java/com/bhca/transaction/exception/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.bhca.transaction.exception;
 
+import org.openapitools.client.model.ErrorView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -16,12 +18,30 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Object handleServerException(Exception e) {
-        return convert(HttpStatus.INTERNAL_SERVER_ERROR, e);
+    public ErrorView handleServerException(Exception e) {
+        return convert(HttpStatus.INTERNAL_SERVER_ERROR, e,
+                "Unexpected error occurred. Sorry for inconvenience.");
     }
 
-    private Object convert(HttpStatus httpStatus, Exception e) {
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorView handleBadRequestException(NoHandlerFoundException e) {
+        return convert(HttpStatus.NOT_FOUND, e, "No such path exist.");
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorView handleBadRequestException(IllegalArgumentException e) {
+        return convert(HttpStatus.BAD_REQUEST, e, "Bad request.");
+    }
+
+    private ErrorView convert(HttpStatus httpStatus, Exception e, String message) {
         logger.error(httpStatus.getReasonPhrase(), e);
-        return new ErrorView();
+        ErrorView errorView = new ErrorView();
+        errorView.setMessage(message);
+        errorView.setDetails(e.getMessage());
+        return errorView;
     }
 }
