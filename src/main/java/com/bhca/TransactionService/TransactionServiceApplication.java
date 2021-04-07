25eb6783891d5cff334140ef472c583d75906241
Mikhail Dergalev <mikhail.dergalev@crypterium.com>
2021-04-07 11:53:35 +0300

    Boilerplate commit

---
+++ src/main/java/com/bhca/TransactionService/TransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bhca.TransactionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionServiceApplication.class, args);
+	}
+
+}
